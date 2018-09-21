559da3c56db267e6b7ebeb815a66ef9869b9ef25
Belle <bebold.bebelle@gmail.com>
2018-09-20 21:20:58 -0400

    Put all my characters in a character object and used one route isntead of three.

--- server.js
+++ server.js
@@ -2,35 +2,38 @@
 var express = require("express");
 var app = express();
 // var bodyParser = require("body-parser");
-
 var PORT = 3000;
-
-//Data
-var yoda = {
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  };
-  
-  var darthmaul = {
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  };
-
-  var obi = {
-    name: "Obi Wan Kenobi",
-    role: "Sith Lord",
-    age:350,
-    forcePoints: 1500
-  };
-
 
 //Sets up BodyParser
 //   app.use(bodyParser.urlencoded({ extended: false }))
 //   app.use(bodyParser.json())
+
+//Data
+var character = [
+    {
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+    },
+    {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+    },
+    {
+    routeName: " Obi",
+    name: "Obi Wan Kenobi",
+    role: "Sith Lord",
+    age:350,
+    forcePoints: 1500
+  }];
+
+
+
 
   //Routes
   app.get('/', function(req, res){
@@ -38,19 +41,27 @@ var yoda = {
       console.log("Welcome to the start Wars Page!");
   });
 
-  app.get("/yoda", function(req, res) {
-    res.json(yoda);
-  });
+  app.get("/:character", function(req, res){
+      console.log("This is my first character " + character[0].name);
+      var chosen = req.params.character[0].name;
+      console.log(chosen);
+      res.end();
+  })
 
-  app.get('/darthmaul', function(req, res){
-      res.json(darthmaul);
-      console.log("This is my Darthmaul Page!");
-  });
+//These are the individual routes if we were using each charater as their own variable instead of a global Character object for all of them.
+//   app.get("/yoda", function(req, res) {
+//     res.json(yoda);
+//   });
 
-  app.get('/obi', function(req, res){
-    res.json(obi);
-    console.log("This is my Obi Page!");
-});
+//   app.get('/darthmaul', function(req, res){
+//       res.json(darthmaul);
+//       console.log("This is my Darthmaul Page!");
+//   });
+
+//   app.get('/obi', function(req, res){
+//     res.json(obi);
+//     console.log("This is my Obi Page!");
+// });
 
 
 //App is listening
