5c418a1f7cbbba7aa6297520f2c58cd287730737
Belle <bebold.bebelle@gmail.com>
2018-09-20 20:39:33 -0400

    Created Repo. Installed npm packages. Started the server. Established 3 routes.

---
+++ server.js
@@ -0,0 +1,60 @@
+//Dependencies
+var express = require("express");
+var app = express();
+// var bodyParser = require("body-parser");
+
+var PORT = 3000;
+
+//Data
+var yoda = {
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  };
+  
+  var darthmaul = {
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  };
+
+  var obi = {
+    name: "Obi Wan Kenobi",
+    role: "Sith Lord",
+    age:350,
+    forcePoints: 1500
+  };
+
+
+//Sets up BodyParser
+//   app.use(bodyParser.urlencoded({ extended: false }))
+//   app.use(bodyParser.json())
+
+  //Routes
+  app.get('/', function(req, res){
+      res.send("Welcome to the start Wars Page!");
+      console.log("Welcome to the start Wars Page!");
+  });
+
+  app.get('/yoda', function(req,res){
+      res.json(yoda);
+      console.log("This is my Yoda Page!");
+  });
+
+  app.get('/darthmaul', function(req, res){
+      res.json(darthmaul);
+      console.log("This is my Darthmaul Page!");
+  });
+
+  app.get('/obi', function(req, res){
+    res.json(obi);
+    console.log("This is my ObiPage!");
+});
+
+
+//App is listening
+app.listen(PORT, function(){
+    console.log("My app is listening on port " + PORT);
+})
